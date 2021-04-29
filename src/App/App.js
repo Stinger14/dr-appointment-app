30fc5dfc889d8e04b38c20a3161e62147a22cae1
Maxly Garcia <maxgarcia6@gmail.com>
2021-04-29 02:17:49 -0400

    Add SideMenu component, define app layout

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import SideMenu from '../components/SideMenu';
+import './App.css';
+
+function App() {
+  return (
+    // <></> === React.Fragment
+    <>  
+    <SideMenu/>
+    <div>Home Page</div>
+    </>
+  );
+}
+
+export default App;
